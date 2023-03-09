5f874c97007f6b337f36d14b69edd6638dc15287
Luke Lynch <luke@lukelynch.io>
2023-03-10 03:18:45 +1100

    Added new files

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<template>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="icon" href="../../dist/favicon.ico">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Vite App</title>
+    </head>
+    
+</template>
