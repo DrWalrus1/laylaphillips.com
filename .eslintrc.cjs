687803b5c8e130f186c0c83962c3254c4e9c29c0
Luke Lynch <luke@lukelynch.io>
2023-03-10 05:52:36 +1100

    minor changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 /* eslint-env node */
 module.exports = {
   root: true,
+  "env": {
+    "node": true,
+    "commonjs": false
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended'
