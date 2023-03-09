c916467294025420b86bc6283dddfded12d29861
Luke Lynch <luke@lukelynch.io>
2023-03-10 06:10:25 +1100

    so stupid

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/laylaphillips.com.git/',
+    base: '/laylaphillips.com/',
     plugins: [vue()],
     resolve: {
         alias: {
